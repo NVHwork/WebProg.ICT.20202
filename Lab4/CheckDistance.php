680aaa72f12ed53871da5398eca6d5c22f5e81e2
nvhwork <hoang.nv176762@sis.hust.edu.vn>
2021-03-24 14:18:13 +0700

    Lab 4: Check Distance

---
+++ Lab4/CheckDistance.php
@@ -0,0 +1,47 @@
+<html>
+	<head>
+		<title>Distance and Time Calculations</title>
+	</head>
+
+	<body>
+		<?php
+			$cities = array('Dallas' => 803,
+							'Toronto' => 435,
+							'Boston' => 848,
+							'Nashville' => 406,
+							'Las Vegas' => 1526,
+							'San Francisco' => 1835,
+							'Washington, DC' => 595,
+							'Miami' => 1189,
+							'Pittsburgh' => 409);
+			$destination = $_POST['destination'];
+			// print (count($destination));
+			function calcDriveTime ($ct, $dest) {
+				return round( ($ct[$dest] / 60) , 2 );
+			}
+			function calcWalkTime ($ct, $dest) {
+				return round( ($ct[$dest] / 5) , 2 );
+			}
+		?>
+		<table border=5>
+			<thead>
+				<th>No.</th>
+				<th>Destination</th>
+				<th>Distance</th>
+				<th>Driving time</th>
+				<th>Walking time</th>
+			</thead>
+			<?php
+				for ($i = 0; $i < sizeof($destination); $i++) {
+					// print $destination[$i]. "<br>";
+					$d = $destination[$i];
+					print "<tr><td>" . $i+1 . "</td>";
+					print "<td>$d</td>";
+					print "<td>$cities[$d]</td>";
+					print "<td>". calcDriveTime($cities, $d) ."</td>";
+					print "<td>". calcWalkTime($cities, $d) ."</td></tr>";
+				}
+			?>
+		</table>
+	</body>
+</html>
