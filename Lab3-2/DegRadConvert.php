ec47f3edd6baee71abe3f0c618956401a9f0c8a9
phucnguyen111 <phucnguyen0111@gmail.com>
2021-03-27 19:15:04 +0700

    fix undefined

--- Lab3-2/DegRadConvert.php
+++ Lab3-2/DegRadConvert.php
@@ -1,36 +1,44 @@
 <html>
-	<head>
-		<title>Degree - Radian Converter</title>
-	</head>
 
-	<body>
-		<h2>Degree - Radian Converter</h2><hr>
-		<form action="" method="post">
-			<label for="number">Input number and select the unit!</label><br>
-			<input type="number" name="number">
-			<select name="unit">
-				<option value="degrees">degrees</option>
-				<option value="radians">radians</option>
-			</select><br><br>
-			<button type="submit">Submit</button>
-			<button type="reset">Reset</button><hr>
+<head>
+	<title>Degree - Radian Converter</title>
+</head>
 
-			<?php
-				$number = $_POST["number"];
-				$unit = $_POST["unit"];
-				if (is_numeric($number)) {
-					if ($unit == "degrees") { // Convert degree to radian
-						$convertVal = $number / 180 * pi();
-						$convertUnit = "radians";
-					} else { // Convert radian to degree
-						$convertVal = $number * 180 / pi();
-						$convertUnit = "degrees";
-					}
-					echo "$number $unit = $convertVal $convertUnit";
-				} else {
-					print ("Your input is not numeric. Please input a number!!!");
+<body>
+	<h2>Degree - Radian Converter</h2>
+	<hr>
+	<form action="" method="post">
+		<label for="number">Input number and select the unit!</label><br>
+		<input type="number" name="number">
+		<select name="unit">
+			<option value="degrees">degrees</option>
+			<option value="radians">radians</option>
+		</select><br><br>
+		<button type="submit">Submit</button>
+		<button type="reset">Reset</button>
+		<hr>
+
+		<?php
+		$number = '';
+		$unit = '';
+		if (isset($_POST['number'], $_POST['unit'])) {
+			$number = $_POST["number"];
+			$unit = $_POST["unit"];
+			if (is_numeric($number)) {
+				if ($unit == "degrees") { // Convert degree to radian
+					$convertVal = $number / 180 * pi();
+					$convertUnit = "radians";
+				} else { // Convert radian to degree
+					$convertVal = $number * 180 / pi();
+					$convertUnit = "degrees";
 				}
-			?>
-		</form>
-	</body>
+				echo "$number $unit = $convertVal $convertUnit";
+			} else {
+				print("Your input is not numeric. Please input a number!!!");
+			}
+		}
+		?>
+	</form>
+</body>
+
 </html>
